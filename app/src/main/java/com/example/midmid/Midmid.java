3f25f6a2bb39a759b2164e7b76384f320713c7a8
CSuksudprasert <blackstones5054@gmail.com>
2018-10-13 16:06:33 +0700

    Initial commit

---
+++ app/src/main/java/com/example/midmid/Midmid.java
@@ -0,0 +1,13 @@
+package com.example.midmid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Midmid extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_midmid);
+    }
+}
